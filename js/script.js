3d6f45e67ef058a66d24b3dbf2cded35e53aa19a
manfred-commits <manfredipiraino@gmail.com>
2021-09-07 16:02:06 +0200

    aggiunto oggetto e stampa a schermo

--- js/script.js
+++ js/script.js
@@ -4,3 +4,25 @@
 // Creare un array di oggetti di studenti.
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+
+
+
+
+
+// 1.Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+
+var studente={
+    "nome" : "Manfredi",
+    "cognome" : "Piraino",
+    "età" : 25
+}
+
+console.log(studente);
+
+for(var key in studente){
+    alert(studente[key]);
+    console.log(studente[key]);
+
+}
